339d81bd7e14016367c285df8c918d2eed01c663
Rebecca The Great <rebbe01@gmail.com>
2025-03-26 20:44:12 +0100

    touch controls and restructure, each element has its own folder with scripts, scenes, sprites

---
+++ player/scripts/touch_controller.gd
@@ -0,0 +1,82 @@
+# ProtoController v1.0 by Brackeys
+# CC0 License
+# Intended for rapid prototyping of first-person games.
+# Happy prototyping!
+
+extends CharacterBody3D
+
+## Can we move around?
+@export var can_move : bool = true
+## Are we affected by gravity?
+@export var has_gravity : bool = true
+
+@export_group("Speeds")
+## Normal speed.
+@export var base_speed : float = 50.0
+
+@export_group("Input Actions")
+## Name of Input Action to move Left.
+@export var input_left : String = "ui_left"
+## Name of Input Action to move Right.
+@export var input_right : String = "ui_right"
+## Name of Input Action to move Forward.
+@export var input_forward : String = "ui_up"
+## Name of Input Action to move Backward.
+@export var input_back : String = "ui_down"
+
+var mouse_captured : bool = false
+var look_rotation : Vector2
+var move_speed : float = 0.0
+var freeflying : bool = false
+
+## IMPORTANT REFERENCES
+@onready var head: Node3D = $Head
+@onready var collider: CollisionShape3D = $Collider
+
+func _ready() -> void:
+	check_input_mappings()
+	look_rotation.y = rotation.y
+	look_rotation.x = head.rotation.x
+
+
+func _physics_process(delta: float) -> void:	
+	# Apply gravity to velocity
+	if has_gravity:
+		if not is_on_floor():
+			velocity += get_gravity() * delta
+
+		move_speed = base_speed
+
+	# Apply desired movement to velocity
+	if can_move:
+		var input_dir := Input.get_vector(input_left, input_right, input_forward, input_back)
+		var move_dir := (head.global_basis * Vector3(-input_dir.x, 0, -input_dir.y)).normalized()
+		if move_dir:
+			velocity.x = move_dir.x * move_speed
+			velocity.z = move_dir.z * move_speed
+		else:
+			velocity.x = move_toward(velocity.x, 0, move_speed)
+			velocity.z = move_toward(velocity.z, 0, move_speed)
+	else:
+		velocity.x = 0
+		velocity.y = 0
+	
+	# Use velocity to actually move
+	move_and_slide()
+
+
+## Checks if some Input Actions haven't been created.
+## Disables functionality accordingly.
+func check_input_mappings():
+	if can_move and not InputMap.has_action(input_left):
+		push_error("Movement disabled. No InputAction found for input_left: " + input_left)
+		can_move = false
+	if can_move and not InputMap.has_action(input_right):
+		push_error("Movement disabled. No InputAction found for input_right: " + input_right)
+		can_move = false
+	if can_move and not InputMap.has_action(input_forward):
+		push_error("Movement disabled. No InputAction found for input_forward: " + input_forward)
+		can_move = false
+	if can_move and not InputMap.has_action(input_back):
+		push_error("Movement disabled. No InputAction found for input_back: " + input_back)
+		can_move = false
