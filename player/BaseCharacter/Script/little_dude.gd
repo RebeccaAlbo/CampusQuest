d6741b680c3147e03ca943d1c43507dd103ef5ab
amandaaath <amanda.thoren@outlook.com>
2025-03-30 15:01:20 +0200

    First walk on campus
    Character imported, coded to move and walk

---
+++ player/BaseCharacter/Script/little_dude.gd
@@ -0,0 +1,46 @@
+extends CharacterBody3D
+
+@onready var armature: Node3D = $Armature
+@onready var spring_arm_pivot: Node3D = $SpringArmPivot
+@onready var spring_arm: SpringArm3D = $SpringArmPivot/SpringArm3D
+@onready var anim_tree: AnimationTree = $AnimationTree
+
+const SPEED = 20.0
+const LERP_VAL = .15
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+		
+	# Mouse control viewpoint
+	if event is InputEventMouseMotion:
+		spring_arm_pivot.rotate_y(-event.relative.x * .001)
+		spring_arm.rotate_x(-event.relative.y * .001)
+		# No infinite rotation
+		spring_arm.rotation.x = clamp(spring_arm.rotation.x, -PI/4, PI/4)
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("left", "right", "up", "down")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	# Mouse control direction of character
+	direction = direction.rotated(Vector3.UP, spring_arm_pivot.rotation.y)
+	if direction:
+		velocity.x = lerp(velocity.x, direction.x * SPEED, LERP_VAL)
+		velocity.z = lerp(velocity.z, direction.z * SPEED, LERP_VAL)
+		armature.rotation.y = lerp_angle(armature.rotation.y, atan2(-velocity.x, -velocity.z), LERP_VAL)
+	else:
+		velocity.x = lerp(velocity.x, 0.0, LERP_VAL)
+		velocity.z = lerp(velocity.z, 0.0, LERP_VAL)
+		
+	anim_tree.set("parameters/BlendSpace1D/blend_position", velocity.length() / SPEED)
+
+	move_and_slide()
